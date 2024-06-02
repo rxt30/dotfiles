f0fa64743276d20b2b5e465c478d40467484d287
rxt30 <mhampel@pm.me>
2024-06-02 11:50:03 +0200

    Added conda init

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,3 +9,17 @@ if status is-interactive
 	alias vi="vim"
       end
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/maxh/miniconda3/bin/conda
+    eval /home/maxh/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/maxh/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/maxh/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/maxh/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
